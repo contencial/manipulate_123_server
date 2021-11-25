c174d7903c68abff1d570384851bd9d936cfba3b
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-11-25 19:51:01 +0900

    fix: 不要なライブラリのインポートを削除

--- check_http_status.py
+++ check_http_status.py
@@ -1,16 +1,10 @@
 import os
 import re
-import pathlib        
 import datetime
 import requests
 import gspread
 from time import sleep
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
-from fake_useragent import UserAgent
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Logger setting
@@ -72,74 +66,6 @@ def write_response(response):
         i += 1
     sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
 
-def create_message(response):
-    flag = False
-    message = 'ご担当者さま\n\n'
-    message += '現時点でダウンしているドメイン一覧です。\n\n'
-    message += 'ご確認お願い致します。\n\n'
-    message += 'サーバー番号 メインドメイン HTTPステータスコード 内容\n'
-    for element in response:
-        if element[2] == 200 and element[3] == 'ERROR':
-            continue ;
-        else:
-            flag = True
-            message += f'{element[0]} {element[1]} {element[2]} {element[3]}\n'
-    if flag == True:
-        return message
-    else:
-        return None
-
-def button_click(driver, button_text):
-    buttons = driver.find_elements_by_tag_name("button")
-
-    for button in buttons:
-        if button.text == button_text:
-            button.click()
-            break
-
-def create_issue(message):
-    url = "https://member.123server.jp/members/login/"
-    login = os.environ['SERVER123_USER']
-    password = os.environ['SERVER123_PASS']
-    webdriverPath = os.environ['WEBDRIVER_PATH']
-
-    ua = UserAgent()
-    logger.debug(f'create_issue: UserAgent: {ua.chrome}')
-
-    options = Options()
-    options.add_argument(f'user-agent={ua.chrome}')
-
-    try:
-        driver = webdriver.Chrome(executable_path=webdriverPath, options=options)
-
-        driver.get(url)
-        driver.maximize_window()
-
-        driver.find_element_by_id("MemberContractId").send_keys(login)
-        driver.find_element_by_id("MemberPassword").send_keys(password)
-        button_click(driver, "ログイン")
-
-        logger.debug('create_issue: login')
-        sleep(3)
-
-        driver.find_element_by_xpath('//li[@class="accordion"][2]').click()
-        sleep(2)
-        driver.find_element_by_xpath('//a[@href="/tickets/open"]').click()
-
-        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        driver.implicitly_wait(60)
-        driver.find_element_by_id("TicketTicketTitle").send_keys(f'現時点でダウンしているドメイン一覧 {now}')
-        driver.find_element_by_id("TicketBody").send_keys(message)
-        sleep(10)
-        driver.find_element_by_xpath('//input[@type="submit"]').click()
-        sleep(10)
-
-        driver.close()
-        driver.quit()
-    except Exception as err:
-        logger.debug(f'Error: create_issue: {err}')
-        exit(1)
-
 ### main_script ###
 if __name__ == '__main__':
 
@@ -149,10 +75,6 @@ if __name__ == '__main__':
         logger.debug("check_http_status: start http_request")
         response = list(http_request(domain_info))
         write_response(response)
-        message = create_message(response)
-        logger.debug(message)
-#        if not message == None:
-#            create_issue(message)
         exit(0)
     except Exception as err:
         logger.debug(f'check_http_status: {err}')
